12a54509a749d4561cc943181158728e6fd9865f
RafaS92 <manrvaldez..92@gmail.com>
2020-04-07 19:29:28 -0500

    final2.0

--- app/controllers/dogs_controller.rb
+++ app/controllers/dogs_controller.rb
@@ -1,2 +1,14 @@
 class DogsController < ApplicationController
+
+    def index
+        @dogs = Dog.all
+        # byebug
+        @dogs_order = @dogs.sort_by { |dog| dog.employees.length }
+        
+    end
+
+    def show
+        @dog = Dog.find(params[:id])
+    end
+
 end
