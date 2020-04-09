9c8b6b8456e51ebd0c6b769ce874b29f5ce9fc83
bryantomoregie <bryantomoregie@yahoo.com>
2020-04-08 23:18:07 -0500

    completed

--- app/controllers/dogs_controller.rb
+++ app/controllers/dogs_controller.rb
@@ -1,2 +1,14 @@
 class DogsController < ApplicationController
+    
+    def index 
+        @dogs = Dog.all
+    end 
+
+    def show 
+        @dog = Dog.find(params[:id])
+        # byebug 
+        @employees = @dog.employees
+
+    end 
+
 end
