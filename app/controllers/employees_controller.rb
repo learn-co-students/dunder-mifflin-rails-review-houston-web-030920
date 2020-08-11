cd235cbbd3087ff1d0714418ff895935041547c9
angeloxenakis <angeloxenakis@icloud.com>
2020-08-11 17:21:45 -0500

    test

--- app/controllers/employees_controller.rb
+++ app/controllers/employees_controller.rb
@@ -1,2 +1,17 @@
 class EmployeesController < ApplicationController
+
+    def index 
+        @employees = Employee.all 
+    end
+
+    def show
+        @employee = Employee.find(params[:id])
+        @dog = Dog.find(@employee.dog_id)
+    end
+
+    def new
+    end
+
+    def edit
+    end
 end
